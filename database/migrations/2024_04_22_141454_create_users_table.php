9603390a05107ed336051dbf5b54cd44bd1d1bf5
pfernandez <pedrofernbel@gmail.com>
2024-04-22 17:08:50 +0200

    feat: migrations added.

--- database/migrations/2024_04_22_141454_create_users_table.php
+++ database/migrations/2024_04_22_141454_create_users_table.php
@@ -15,12 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nickname')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->unsignedBigInteger('role_id');
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            // $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('role')->onDelete('cascade');
         });
     }
 
