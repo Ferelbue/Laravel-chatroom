823bead3e96826c31060bf8b462831387bb5a663
Fernando <ferelbue@gmail.com>
2024-04-22 18:40:10 +0200

    feat: migrations files created

--- database/migrations/2024_04_22_162806_create_users_table.php
+++ database/migrations/2024_04_22_162806_create_users_table.php
@@ -16,11 +16,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('nickName');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('is_active');
+            $table->unsignedBigInteger('role_id');
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('role')->onDelete('cascade');
         });
     }
 
