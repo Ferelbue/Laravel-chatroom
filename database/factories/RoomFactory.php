c0f9ccabe3c306d25163a206acc443bde9e0a9d8
pfernandez <pedrofernbel@gmail.com>
2024-04-23 17:38:03 +0200

    feat: added RoomFactory.

---
+++ database/factories/RoomFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class RoomFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(),
+            'user_id' => rand(1, 10),
+            'game_id' => rand(1, 5),
+        ];
+    }
+}
