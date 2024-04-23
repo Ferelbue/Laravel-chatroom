c973b06b840e962144ee65aadecbadca66cc5c53
pfernandez <pedrofernbel@gmail.com>
2024-04-23 17:39:49 +0200

    feat: added ChatFactory.

---
+++ database/factories/ChatFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ChatFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'message' => $this->faker->sentence(),
+            'user_id' => rand(1, 10),
+            'room_id' => rand(1, 5),
+        ];
+    }
+}
