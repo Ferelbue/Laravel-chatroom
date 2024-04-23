d35db98cfd9d6107d93eaec4ffe58ad147a8f608
pfernandez <pedrofernbel@gmail.com>
2024-04-23 19:42:19 +0200

    feat: added getGames route.

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,19 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Models\Game;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
     public function createGame(Request $request)
     {
-        $validatedData = $request->validate([
-            'title' => 'required|max:55',
-            'description' => 'required',
-        ]);
+        try {
+            $validatedData = $request->validate([
+                'title' => 'required|max:55',
+                'description' => 'required',
+            ]);
+    
+            $game = Game::create($validatedData);
+    
+            return response(
+                [
+                    "Success" => true,
+                    "Message" => "Game created successfully",
+                    "Data" => $game
+                ]
+            );
+        } catch (\Throwable $th) {
+            return response(
+                [
+                    "Success" => false,
+                    "Message" => "An error occurred",
+                    "Data" => $th->getMessage()
+                ]
+            );
+        }
+    }
 
-        $game = Game::create($validatedData);
-
-        return response(['game' => $game]);
+    public function getGames()
+    {
+        try {
+            $games = Game::all();
+    
+            return response(['games' => $games]);
+        } catch (\Throwable $th) {
+            return response(
+                [
+                    "Success" => false,
+                    "Message" => "An error occurred",
+                    "Data" => $th->getMessage()
+                ]
+            );
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/games', [GameController::class, 'createGame']);
+Route::get('/games', [GameController::class, 'getGames']);
