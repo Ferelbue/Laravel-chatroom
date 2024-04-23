24b6b4abd0e43cb69b53db117272aab5e86f1f84
pfernandez <pedrofernbel@gmail.com>
2024-04-23 17:35:11 +0200

    feat: added GameFactory.

---
+++ database/factories/GameFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class GameFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->text,
+            'user_id' => rand(1, 10), // We need to change this to match number of users in factory
+        ];
+    }
+}
