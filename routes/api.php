2aac012c4ed3ef7f6335f3a88917294b3cb04e4c
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-04-25 13:44:29 +0200

    feat: add routes users, chats and auth

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ChatController;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\RoomController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 // GAMES
 Route::middleware(['auth:sanctum'])->group(function () {
@@ -21,25 +16,32 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/games/{id}', [GameController::class, 'getGameById']);
 });
 
-// // GAME ROUTES
-// Route::post('/games', [GameController::class, 'createGame']);
-// Route::get('/games', [GameController::class, 'getGames']);
-// Route::put('/games/{id}', [GameController::class, 'editGame']);
-// Route::delete('/games/{id}', [GameController::class, 'deleteGame']);
-
 // ROOM ROUTES
 Route::post('/rooms', [RoomController::class, 'createRoom'])->middleware('auth:sanctum');
 Route::get('/rooms', [RoomController::class, 'getRooms']);
-// Route::put('/rooms/{id}', [RoomController::class, 'joinLeaveRoom']);
 Route::put('/rooms/{id}', [RoomController::class, 'editRoom'])->middleware('auth:sanctum');
 Route::delete('/rooms/{id}', [RoomController::class, 'deleteRoom'])->middleware('auth:sanctum');
 Route::get('/rooms/{id}', [RoomController::class, 'getRoom']);
 Route::post('/rooms/{id}/join', [RoomController::class, 'joinRoom'])->middleware('auth:sanctum');
 Route::post('/rooms/{id}/leave', [RoomController::class, 'leaveRoom'])->middleware('auth:sanctum');
 
-// AUTH ROUTES
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
-
 // CHAT ROUTES
-Route::get('/chats/{roomId}', [ChatController::class, 'getAllChats'])->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/chats', [ChatController::class, 'createChat']);
+    Route::delete('/chats/{id}', [ChatController::class, 'deleteChatById']);
+    Route::get('/chats/{roomId}', [ChatController::class, 'getAllChats']);
+});
+
+// AUTH ROUTES
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::delete('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/profile', [AuthController::class, 'getProfile'])->middleware('auth:sanctum');
+
+// USER ROUTES
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::get('/users', [UserController::class, 'getAllUsers']);
+    Route::post('/users', [UserController::class, 'createUser']);
+    Route::put('/users/{id}', [UserController::class, 'updateUserById']);
+    Route::delete('/users/{id}', [UserController::class, 'deleteUserById']);
+});
